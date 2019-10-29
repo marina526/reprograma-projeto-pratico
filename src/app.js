2321ddae62837b9f0d59a71612467319755f2df7
Roms2019 <romeniaishiyama@gmail.com>
2019-10-28 22:17:20 -0300

    Adicionar server, app, index e lista de tarefas

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+//rotas
+const index = require("./routes/index")
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
+
+app.use("/", index)
+
+module.exports = app
